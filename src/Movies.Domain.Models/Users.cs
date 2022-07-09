526bee567969b3e08d901254f17041412e9cea2c
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-07-09 14:37:01 -0300

    feat: add user domain model

---
+++ src/Movies.Domain.Models/Users.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Movies.Domain.Models;
+
+public class Users {
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public DateOnly BirthDate { get; set; }
+}
+
