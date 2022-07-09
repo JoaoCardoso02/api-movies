72cd7064e3b6fc48a575da9c3d459c7b6bd3bd4a
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-07-09 14:37:38 -0300

    feat: add data layer

---
+++ src/Movies.Application.Infrastructure.Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Domain.Models;
+
+namespace Movies.Application.Infrastructure.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {}
+
+    public DbSet<Users> Users { get; set; }
+}
+
