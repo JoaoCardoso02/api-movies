7b108cb9a9da141416ad1aa6b094f2904633f647
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-07-09 14:38:59 -0300

    feat: add route user

---
+++ src/Movies.API/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Models.User;
+
+namespace Movies.API.Controllers;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/v1/[controller]")]
+public class UserController
+{
+    public UserController()
+    {
+    }
+
+    [HttpPost()]
+    [Consumes(MediaTypeNames.Application.Json)]
+    public CreateUserRequest CreateUser(CreateUserRequest createUserRequest) {
+        return createUserRequest;
+    }
+}
--- src/Movies.API/Program.cs
+++ src/Movies.API/Program.cs
@@ -1,8 +1,15 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿global using Movies.Application.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using MySQL.Data.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options => {
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
