854edda88002f95e33cedeead444e50a1c6bbb15
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-07-12 07:30:41 -0300

    feat: add GetAll method in UserAppService

---
+++ src/Movies.Application/Services/UserAppService.cs
@@ -0,0 +1,26 @@
+﻿using Movies.Domain.Services.Interfaces;
+using Movies.Application.Models.User;
+
+namespace Movies.Application.Services;
+
+public class UserAppService
+{
+    private readonly IUserService _userService;
+
+    public UserAppService(IUserService UserService)
+    {
+        _userService = UserService;
+    }
+
+    public async Task<List<GetAllUserResult>> GetAll() {
+        var users = await _userService.GetAll();
+
+        List<GetAllUserResult> result = new List<GetAllUserResult>();
+
+        users.ForEach(user => {
+            result.Add(new GetAllUserResult(user.Id));
+        });
+
+        return result;
+    }
+}
