e8e263f39a0431c4566f7b2a1981082be79dd8a3
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-07-12 07:28:14 -0300

    feat: add get all user result interface

---
+++ src/Movies.Application.Models/User/GetAllUserResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.Application.Models.User;
+
+public class CreateUserRequest
+{
+    [FromBody]
+    public string Name { get; set; }
+
+    [FromBody]
+    public string Email { get; set; }
+
+    [FromBody]
+    public string Password { get; set; }
+
+    [FromBody]
+    public DateOnly BirthDate { get; set; }
+}
+
