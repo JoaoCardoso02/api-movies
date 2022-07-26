a4e1961f7ab8a13e01f5baca527435e06a72f443
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-07-26 12:48:17 -0300

    feat: add Movie Model

---
+++ src/Movies.Domain.Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Movies.Domain.Models;
+
+public class Movie {
+    public long? Id { get; set; }
+    public string Name { get; set; }
+    public string Synopsis { get; set; }
+    public string Picture { get; set; }
+    public uint Rating { get; set; }
+    public DateTime ReleaseDate { get; set; }
+
+    public Movie(
+        string name,
+        string synopsis,
+        string picture,
+        uint rating,
+        DateTime releaseDate,
+        long? id
+    ) {
+        Name = name;
+        Synopsis = synopsis;
+        Picture = picture;
+        Rating = rating;
+        ReleaseDate = releaseDate;
+        Id = id;
+    }
+
+    public Movie() {}
+}
