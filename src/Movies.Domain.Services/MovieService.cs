b749d20766df9b59335ba40e35e23eee8c67b41a
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-07-27 12:39:06 -0300

    feat: add inital movie service

---
+++ src/Movies.Domain.Services/MovieService.cs
@@ -0,0 +1,15 @@
+﻿using Movies.Domain.Services.Interfaces;
+using Movies.Infrastructure.Data;
+
+namespace Movies.Domain.Services;
+
+public class MovieService : IMovieService
+{
+    private readonly DataContext Repository;
+
+
+    public MovieService(DataContext repository) {
+        Repository = repository;
+    }
+}
+
