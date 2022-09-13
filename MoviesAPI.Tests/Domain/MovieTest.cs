06c0f0efa7ddabdcf2ed3896a44872ad08ecb822
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-09-13 19:04:58 -0300

    test: add MovieTest

---
+++ MoviesAPI.Tests/Domain/MovieTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Linq;
+using Movies.Domain.Models;
+namespace MoviesAPI.Tests;
+
+public class MovieTest {
+    [Test]
+    public void ShouldCreateMovieSuccessfully() {
+        Movie movie = new Movie {
+            Name = "Harry Potter and The Philosopher's Stone",
+            Synopsis = "Harry Potter is a ...",
+            Picture = "https://some-url.com",
+            Rating = 9,
+            ReleaseDate = new DateTime(2001, 11, 23)
+        };
+
+        Assert.IsTrue(movie.Name.Equals("Harry Potter and The Philosopher's Stone"));
+    }
+}
