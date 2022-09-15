ee73160eb3011502166b85c42af26cb5a5e7d0ea
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-09-14 21:45:25 -0300

    feat: improve Movie model test

--- MoviesAPI.Tests/Domain/MovieTest.cs
+++ MoviesAPI.Tests/Domain/MovieTest.cs
@@ -14,5 +14,22 @@ public class MovieTest {
         };
 
         Assert.IsTrue(movie.Name.Equals("Harry Potter and The Philosopher's Stone"));
+        Assert.IsTrue(movie.Synopsis.Equals("Harry Potter is a ..."));
+        Assert.IsTrue(movie.Picture.Equals("https://some-url.com"));
+        Assert.IsTrue(movie.Rating.Equals(9));
+        Assert.IsTrue(movie.ReleaseDate.Equals(new DateTime(2001, 11, 23)));
+    }
+
+    [Test]
+    public void ShouldBeInstanceOfMovie() {
+        Movie movie = new Movie {
+            Name = "Harry Potter and The Philosopher's Stone",
+            Synopsis = "Harry Potter is a ...",
+            Picture = "https://some-url.com",
+            Rating = 9,
+            ReleaseDate = new DateTime(2001, 11, 23)
+        };
+
+        Assert.IsInstanceOf<Movie>(movie);
     }
 }
