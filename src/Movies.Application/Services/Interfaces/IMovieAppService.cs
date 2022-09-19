956959d2b3dbc98c25f6980490cf452ffbb3733a
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-09-19 19:54:02 -0300

    feat: add MovieAppService interface

---
+++ src/Movies.Application/Services/Interfaces/IMovieAppService.cs
@@ -0,0 +1,12 @@
+﻿using Movies.Application.Models.Movie;
+
+namespace Movies.Application.Services.Interfaces;
+
+public interface IMovieAppService
+{
+    List<GetAllMovieResult> GetAll();
+    GetOneMovieResult GetById(long id);
+    CreateMovieResult Create(CreateMovieRequest movie);
+    UpdateMovieResult Update(long id, UpdateMovieRequest movie);
+    bool Delete(long id);
+}
