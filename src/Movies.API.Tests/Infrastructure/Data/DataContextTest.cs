a97547c6ca10800f7e7b6804d348cd06aba5f4b5
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-09-20 19:21:39 -0300

    refactor: change Movie.API.Tests location

---
+++ src/Movies.API.Tests/Infrastructure/Data/DataContextTest.cs
@@ -0,0 +1,21 @@
+﻿using Movies.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MoviesAPI.Tests.Infrastructure;
+
+public class DataContextTest {
+    [Test]
+    public void ShouldInstaceOfDataContextSucessfully() {
+        var dataContext = new DataContext();
+
+        Assert.IsInstanceOf<DataContext>(dataContext);
+    }
+
+    [Test]
+    public void ShouldInstaceOfDbContextSucessfully() {
+        DataContext dataContext = new DataContext();
+
+        Assert.IsInstanceOf<DbContext>(dataContext);
+    }
+}
+
